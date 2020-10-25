fc522c82f0f23b41b19f405d09126b87b206f037
BalashovPavel <48068717+BalashovPavel@users.noreply.github.com>
2020-10-25 22:38:38 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let parentheses=[];
+  
+  bracketsConfig.forEach(element => { 
+    parentheses.push(element[0]+element[1])
+  });
+
+  for (let i = 0; i < parentheses.length; i++) {
+    if (str.includes(parentheses[i])) {
+        str = str.replace(parentheses[i], '')
+        i = - 1;
+    }
+}
+
+return str === ''
 }
